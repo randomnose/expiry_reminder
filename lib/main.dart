0a134dab90b375dd9f8e3606a408f612157a4ff1
Eson Tan <tanesonjpg@gmail.com>
2021-04-07 14:00:30 +0800

    - added base structure (usable for ios and android)

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:expiry_reminder/models/user.dart';
+import 'package:expiry_reminder/screens/wrapper.dart';
+import 'package:expiry_reminder/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:expiry_reminder/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'authenticate/authenticate.dart';
+import 'home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print('Current user is:');
+    print(user);
+
+    // return either the Home or Authenticate widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+  }
+}
